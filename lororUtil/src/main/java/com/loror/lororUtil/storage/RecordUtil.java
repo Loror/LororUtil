d24379bc44e0dc6b85c444d8826f4d45efb468d3
Loror <Lorrer@126.com>
2018-04-12 16:46:37 +0800

    Loror

--- lororUtil/src/main/java/com/loror/lororUtil/storage/RecordUtil.java
+++ lororUtil/src/main/java/com/loror/lororUtil/storage/RecordUtil.java
@@ -7,124 +7,124 @@ import android.os.Handler;
 import android.os.Looper;
 
 public class RecordUtil {
-	private MediaRecorder recorder;// 录音
-	private Handler handler;
-	private Thread thread;// 监听线程
-	private long startTime;// 录音开始时间
-	private boolean isRecording;// 正在录音
-	private String path;// 录音存储路径
+    private MediaRecorder recorder;// 录音
+    private Handler handler;
+    private Thread thread;// 监听线程
+    private long startTime;// 录音开始时间
+    private boolean isRecording;// 正在录音
+    private String path;// 录音存储路径
 
-	/**
-	 * 音量监听接口
-	 */
-	public interface OnVolumListener {
-		void onVolume(double amplitude);
-	}
+    /**
+     * 音量监听接口
+     */
+    public interface OnVolumListener {
+        void onVolume(double amplitude);
+    }
 
-	/**
-	 * 初始化
-	 */
-	private RecordUtil() {
-		handler = new Handler(Looper.getMainLooper());
-	}
+    /**
+     * 初始化
+     */
+    public RecordUtil() {
+        handler = new Handler(Looper.getMainLooper());
+    }
 
-	/**
-	 * 单例工厂类
-	 */
-	private static class SingleFactory {
-		private static RecordUtil instance = new RecordUtil();
-	}
+    /**
+     * 获取录音存储路径
+     */
+    public String getPath() {
+        return path;
+    }
 
-	/**
-	 * 获取实例
-	 */
-	public static RecordUtil getInstance() {
-		return SingleFactory.instance;
-	}
+    /**
+     * 获取当前录音音量
+     */
+    public double getAmplitude() throws Exception {
+        if (isRecording) {
+            return (recorder.getMaxAmplitude() / 2700.0);
+        } else {
+            return 0;
+        }
+    }
 
-	/**
-	 * 获取录音存储路径
-	 */
-	public String getPath() {
-		return path;
-	}
+    /**
+     * 开始录音
+     */
+    public void start(String path, final OnVolumListener listenser) {
+        if (isRecording) {
+            throw new IllegalStateException("you must call stop before you call another start");
+        }
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        recorder.setAudioEncodingBitRate(196608);
+        recorder.setOutputFile(this.path = path);
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("unexpected path");
+        } catch (Throwable e) {
+            e.printStackTrace();
+            throw new IllegalStateException("may lost permission");
+        }
+        recorder.start();
+        startTime = System.currentTimeMillis();
+        isRecording = true;
+        if (listenser != null) {
+            thread = new Thread() {
+                public void run() {
+                    while (isRecording) {
+                        handler.post(new Runnable() {
 
-	/**
-	 * 获取当前录音音量
-	 */
-	public double getAmplitude() throws Exception {
-		if (isRecording) {
-			return (recorder.getMaxAmplitude() / 2700.0);
-		} else {
-			return 0;
-		}
-	}
+                            @Override
+                            public void run() {
+                                try {
+                                    listenser.onVolume(getAmplitude());
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                        try {
+                            sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                }
 
-	/**
-	 * 开始录音
-	 */
-	public void start(String path, final OnVolumListener listenser) {
-		if (isRecording) {
-			throw new IllegalStateException("you must call stop before you call another start");
-		}
-		recorder = new MediaRecorder();
-		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-		recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-		recorder.setAudioEncodingBitRate(196608);
-		recorder.setOutputFile(this.path = path);
-		try {
-			recorder.prepare();
-			recorder.start();
-			startTime = System.currentTimeMillis();
-			isRecording = true;
-			thread = new Thread() {
-				public void run() {
-					while (isRecording) {
-						handler.post(new Runnable() {
+                ;
+            };
+            thread.start();
+        }
+    }
 
-							@Override
-							public void run() {
-								if (listenser != null) {
-									try {
-										listenser.onVolume(getAmplitude());
-									} catch (Exception e) {
-										e.printStackTrace();
-									}
-								}
-							}
-						});
-						try {
-							sleep(50);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-							break;
-						}
-					}
-				};
-			};
-			thread.start();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-			throw new IllegalStateException("file may not be found");
-		}
-	}
-
-	/**
-	 * 停止录音
-	 */
-	public long stop() {
-		if (!isRecording || recorder == null) {
-			return 0;
-		}
-		isRecording = false;
-		if (thread != null) {
-			thread.interrupt();
-			thread = null;
-		}
-		recorder.stop();
-		recorder.reset();
-		recorder.release();
-		return System.currentTimeMillis() - startTime;
-	}
+    /**
+     * 停止录音
+     */
+    public long stop() {
+        if (!isRecording || recorder == null)
+            return 0;
+        isRecording = false;
+        if (thread != null) {
+            thread.interrupt();
+            thread = null;
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+            return System.currentTimeMillis() - startTime;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 }
--- lororUtil/src/main/java/com/loror/lororUtil/storage/ScanUtil.java
+++ lororUtil/src/main/java/com/loror/lororUtil/storage/ScanUtil.java
@@ -5,103 +5,134 @@ import java.util.Locale;
 
 import com.loror.lororUtil.flyweight.ObjectPool;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Handler;
+import android.provider.MediaStore;
 
 public class ScanUtil {
-	private Handler handler;
+    private Handler handler;
 
-	public interface SDCardCallBack {
-		void scanOne(int find, int scaned);
+    public interface ScanCallBack {
+        void findOne(String path, int find, int scaned);
 
-		void findOne(String path);
+        void finish();
+    }
 
-		void finish();
-	}
+    public ScanUtil() {
+        handler = ObjectPool.getInstance().getHandler();
+    }
 
-	private ScanUtil() {
-		handler = ObjectPool.getInstance().getHandler();
-	}
+    /**
+     * 获取SD卡所有图片
+     */
+    public void scanProviderImages(final Context context, final ScanCallBack callBack) {
+        new Thread() {
+            @Override
+            public void run() {
+                int count = 0;
 
-	private static class SingletonFactory {
-		private static ScanUtil instance = new ScanUtil();
-	}
+                Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                ContentResolver mContentResolver = context.getContentResolver();
 
-	public static ScanUtil getInstance() {
-		return SingletonFactory.instance;
-	}
+                // 只查询jpeg,png,gif的图片
+                Cursor mCursor = mContentResolver.query(mImageUri, null,
+                        MediaStore.Images.Media.MIME_TYPE + "=? or " + MediaStore.Images.Media.MIME_TYPE + "=? or "
+                                + MediaStore.Images.Media.MIME_TYPE + "=?",
+                        new String[]{"image/jpeg", "image/png", "image/gif"}, MediaStore.Images.Media.DATE_MODIFIED);
+                if (mCursor != null) {
+                    while (mCursor.moveToNext()) {
+                        // 获取图片的路径
+                        final String path = mCursor.getString(mCursor.getColumnIndex(MediaStore.Images.Media.DATA));
+                        count++;
+                        final int finalCount = count;
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                callBack.findOne(path, finalCount, 0);
+                            }
+                        });
+                    }
+                    mCursor.close();
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callBack.finish();
+                    }
+                });
+            }
+        }.start();
 
-	/**
-	 * 扫描sd卡，传入参数，1，起点目录，2，需扫描后缀数组，3，回调接口
-	 */
-	public void scanSDCard(final File rootFileDir, final String[] suffixs, final SDCardCallBack callBack) {
-		new Thread() {
-			public void run() {
-				count = 0;
-				total = 0;
-				inintPaths(rootFileDir.listFiles(), suffixs, callBack);
-				handler.post(new Runnable() {
+    }
 
-					@Override
-					public void run() {
-						callBack.finish();
-					}
-				});
-			};
-		}.start();
-	}
+    /**
+     * 扫描sd卡，传入参数，1，起点目录，2，需扫描后缀数组，3，回调接口
+     */
+    public void scanSDCard(final File rootFileDir, final String[] suffixs, final ScanCallBack callBack) {
+        new Thread() {
+            public void run() {
+                inintPaths(rootFileDir.listFiles(), suffixs, callBack);
+                handler.post(new Runnable() {
 
-	private int total;// 总扫描数
-	private int count;// 扫描出目标数
+                    @Override
+                    public void run() {
+                        callBack.finish();
+                    }
+                });
+            }
+        }.start();
+    }
 
-	/**
-	 * 递归扫描
-	 */
-	private void inintPaths(final File[] files, String[] suffixs, final SDCardCallBack callBack) {
-		if (files != null && files.length > 0) {
-			for (int i = 0; i < files.length; i++) {
-				if (files[i].isFile()) {
-					final int index = i;
-					if (contains(files[i].getName(), suffixs)) {
-						handler.post(new Runnable() {
+    /**
+     * 递归扫描
+     */
+    private void inintPaths(final File[] files, String[] suffixs, final ScanCallBack callBack) {
+        int total = 0;
+        int count = 0;
+        if (files != null && files.length > 0) {
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isFile()) {
+                    final int index = i;
+                    total++;
+                    if (contains(files[i].getName(), suffixs)) {
+                        count++;
+                        final int finalCount = count;
+                        final int finalTotal = total;
+                        handler.post(new Runnable() {
 
-							@Override
-							public void run() {
-								callBack.findOne(files[index].getAbsolutePath());
-							}
-						});
-						count++;
-					}
-					handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                callBack.findOne(files[index].getAbsolutePath(), finalCount, finalTotal);
+                            }
+                        });
+                    }
+                } else {
+                    try {
+                        String get = files[i].getName();
+                        if (!get.startsWith(".")) {
+                            inintPaths(files[i].listFiles(), suffixs, callBack);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
-						@Override
-						public void run() {
-							callBack.scanOne(count, total++);
-						}
-					});
-				} else {
-					try {
-						String get = files[i].getName();
-						if (!get.startsWith(".")) {
-							inintPaths(files[i].listFiles(), suffixs, callBack);
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * 检查后缀
-	 */
-	private boolean contains(String name, String[] suffixs) {
-		String uperName = name.toUpperCase(Locale.CHINA);
-		for (int i = 0; i < suffixs.length; i++) {
-			if (uperName.endsWith(suffixs[i].toUpperCase(Locale.CHINA))) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * 检查后缀
+     */
+    private boolean contains(String name, String[] suffixs) {
+        String uperName = name.toUpperCase(Locale.CHINA);
+        for (int i = 0; i < suffixs.length; i++) {
+            if (uperName.endsWith(suffixs[i].toUpperCase(Locale.CHINA))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
