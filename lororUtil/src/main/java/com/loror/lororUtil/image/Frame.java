b511b02f01635992f224ab45297ace2be1f24135
Loror <Lorrer@126.com>
2018-11-07 14:28:39 +0800

    Loror

--- lororUtil/src/main/java/com/loror/lororUtil/image/Frame.java
+++ lororUtil/src/main/java/com/loror/lororUtil/image/Frame.java
@@ -3,12 +3,16 @@ package com.loror.lororUtil.image;
 import android.graphics.Bitmap;
 
 public class Frame {
-	public Bitmap image;
-	public int delay;
-	public Frame nextFrame = null;
+    public Bitmap image;
+    public int delay;
+    public Frame nextFrame = null;
 
-	public Frame(Bitmap im, int del, int widthLimit) {
-		image = BitmapUtil.compessBitmap(im, widthLimit, false);
-		delay = del;
-	}
+    public Frame(Bitmap im, int del, int widthLimit) {
+        if (im.getWidth() > widthLimit) {
+            image = BitmapUtil.compessBitmap(im, widthLimit, false);
+        } else {
+            image = im;
+        }
+        delay = del;
+    }
 }
