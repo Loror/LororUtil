0c6208ba8140d3e193884ee9a8a0f206827d92fd
Loror <34996939+Loror@users.noreply.github.com>
2021-06-17 13:10:27 +0800

    支持修改参数

--- lororUtil/src/main/java/com/loror/lororUtil/http/PacketConverter.java
+++ lororUtil/src/main/java/com/loror/lororUtil/http/PacketConverter.java
@@ -1,8 +1,8 @@
 package com.loror.lororUtil.http;
 
-import java.util.HashMap;
+import java.util.Map;
 
 //打包转换器
 public interface PacketConverter {
-    String convert(String method, HashMap<String, Object> params);
+    String convert(String method, Map<String, Object> params);
 }
