665971b12007ae231b02c7e9ae010f4de86293d0
Loror <34996939+Loror@users.noreply.github.com>
2021-06-20 23:03:03 +0800

    修复视频图片加载

--- lororUtil/src/main/java/com/loror/lororUtil/image/ReadImageUtil.java
+++ lororUtil/src/main/java/com/loror/lororUtil/image/ReadImageUtil.java
@@ -5,9 +5,50 @@ import android.media.MediaMetadataRetriever;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class ReadImageUtil {
+
+    /**
+     * 是否是网络mp4格式
+     */
+    public boolean isNetMp4(String path) {
+        if (path.startsWith("http")) {
+            String url = path;
+            int index = url.lastIndexOf("?");
+            if (index != -1) {
+                url = path.substring(0, index);
+            }
+            return url.toLowerCase(Locale.CHINA).endsWith(".mp4");
+        }
+        return false;
+    }
+
+    /**
+     * 获取网络视频图片
+     */
+    public ReadImageResult getNetMp4ReadImageResult(String path, int widthLimit) {
+        ReadImageResult result = new ReadImageResult();
+        result.setErrorCode(1);//网络下载失败，标注errorCode为1
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(path, new HashMap<String, String>());
+            Bitmap bitmap = retriever.getFrameAtTime();
+            if (bitmap != null) {
+                result.setErrorCode(0);
+                result.addFrame(new Frame(bitmap, 0, widthLimit));
+                result.setOriginPath(path);
+                result.setPath(path);
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            result.setErrorCode(2);
+        } finally {
+            retriever.release();
+        }
+        return result;
+    }
 
     public ReadImageResult getReadImageResult(String path, int widthLimit, boolean mutiCach) {
         return getReadImageResult(path, null, widthLimit, mutiCach);
